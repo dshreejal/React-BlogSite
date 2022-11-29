314d5b365b227b64ab369d7986dddc955283a4a8
Shreejal Dhungana <dshreejal@gmail.com>
2022-11-29 18:39:14 +0545

    Created routes using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home"
+import MyBlogs from "./pages/MyBlogs"
+import { Route, Routes } from 'react-router-dom';
 function App() {
   const client = new QueryClient();
 
   return (
     <QueryClientProvider client={client}>
-      <div>
-
+      <div className=" bg-[#b2e0db] min-h-screen">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/myblogs' element={<MyBlogs />} />
+        </Routes>
+        <Footer />
       </div>
     </QueryClientProvider>
   )
