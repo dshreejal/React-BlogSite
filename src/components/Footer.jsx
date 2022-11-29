4cf7f30d281222a8c5c9c3b57365666a6ee1b805
Shreejal Dhungana <dshreejal@gmail.com>
2022-11-29 18:33:29 +0545

    Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='text-center text-gray-900 text-base absolute bottom-0 w-full'>
+            &copy; 2022  Shreejal Dhungana
+        </div >
+    )
+}
+
+export default Footer
