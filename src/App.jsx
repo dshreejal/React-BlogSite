306508a740efd983306d83c635879c392fafb7ac
Shreejal Dhungana <dshreejal@gmail.com>
2022-11-29 19:58:49 +0545

    blog state

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home"
 import MyBlogs from "./pages/MyBlogs"
 import { Route, Routes } from 'react-router-dom';
+import BlogState from "./context/blogs/BlogState";
 function App() {
   const client = new QueryClient();
 
   return (
     <QueryClientProvider client={client}>
-      <div className=" bg-[#b2e0db] min-h-screen">
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/myblogs' element={<MyBlogs />} />
-        </Routes>
-        <Footer />
-      </div>
+      <BlogState>
+        <div className=" bg-[#b2e0db] min-h-screen">
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/myblogs' element={<MyBlogs />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BlogState>
     </QueryClientProvider>
   )
 }
