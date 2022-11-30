ee65446261da38783d28db674077cf4078d8e47a
Shreejal Dhungana <dshreejal@gmail.com>
2022-11-30 13:27:53 +0545

    added autocomplete for email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,7 +44,6 @@ const Login = () => {
                                         required
                                         className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                         placeholder="Email address"
-                                        autoComplete="off"
                                         value={email} onChange={(e) => setEmail(e.target.value)}
                                     />
                                 </div>
