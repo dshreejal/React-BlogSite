47a8d04ea505599b9f6bb515752983ebfd8f710b
Shreejal Dhungana <dshreejal@gmail.com>
2022-11-30 13:25:34 +0545

    handle sign out and conditional rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { VscSignIn, VscSignOut } from 'react-icons/vsc'
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+    const navigate = useNavigate();
+    const handleLogOut = () => {
+        localStorage.removeItem('auth-token');
+        navigate("/");
+    }
     return (
         <div className='bg-[#399388] flex items-center justify-between px-4 md:px-12 h-[50px]'>
             <div>
@@ -9,18 +15,24 @@ const Navbar = () => {
                     <div className='font-bold text-3xl'>Blog Site</div>
                 </NavLink>
             </div>
-            {/* !!!!!!!!!!!!!! TODO: only render myblog option for logged in user */}
             <div>
-                <NavLink to='/myblogs' className="px-2">
-                    <div className='font-medium text-lg'>My Blogs</div>
-                </NavLink>
+                {localStorage.getItem('auth-token') ?
+                    <NavLink to='/myblogs' className="px-2">
+                        <div className='font-medium text-lg'>My Blogs</div>
+                    </NavLink>
+                    : <div></div>}
             </div>
             <div>
-                {/* !!!!!!!!!!!!!! TODO: Redner LogIn for new users and Signout for logged in users */}
-                <NavLink to='/login' className='flex items-center justify-center gap-1 px-1 font-medium text-lg'>
-                    <div>Log In</div>
-                    <VscSignIn size={20} />
-                </NavLink>
+                {localStorage.getItem('auth-token') ?
+                    <button onClick={handleLogOut} className='flex items-center justify-center gap-1 px-1 font-medium text-lg'>
+                        <div>Sign Out</div>
+                        <VscSignOut size={20} />
+                    </button>
+                    :
+                    <NavLink to='/login' className='flex items-center justify-center gap-1 px-1 font-medium text-lg'>
+                        <div>Log In</div>
+                        <VscSignIn size={20} />
+                    </NavLink>}
             </div>
         </div>
     )
