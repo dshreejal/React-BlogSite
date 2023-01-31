3617b9896d23706b07d1acc224fb4d12a4e9af88
Shreejal Dhungana <dshreejal@gmail.com>
2023-01-31 21:44:06 +0545

    truncate description in blog card  and display author name

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { v4 } from 'uuid';
+import { BsFillPersonFill } from "react-icons/bs"
 const Blog = ({ blog }) => {
     const navigate = useNavigate();
     const handleReadMe = () => {
         const value = v4()
         sessionStorage.setItem('blog-id', blog._id)
         navigate(`/readmore/${value}`)
+    }
+    const truncateString = (str, num) => {
+        if (str?.length > num) {
+            return str.slice(0, num) + '.....'
+        }
+        else {
+            return str;
+        }
     }
     return (
         <>
@@ -17,9 +26,19 @@ const Blog = ({ blog }) => {
                 <div className="p-6">
                     <h5 className="text-gray-900 text-xl font-medium mb-2">{blog.title}</h5>
                     <p className="text-gray-700 text-base mb-4">
-                        {blog.description}
+                        {truncateString(blog?.description, 30)}
                     </p>
-                    <button type="button" className=" inline-block px-6 py-2.5 bg-indigo-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out" onClick={handleReadMe} >Read More</button>
+                    <div className='flex justify-between'>
+                        <button type="button" className=" inline-block px-6 py-2.5 bg-indigo-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out" onClick={handleReadMe} >Read More</button>
+                        <div className='flex items-center justify-center gap-2 text-xs'>
+                            <div>
+                                <BsFillPersonFill />
+                            </div>
+                            <p>
+                                {blog.user.name}
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
