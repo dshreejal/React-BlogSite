85d6d5ba4ca242389c4ead2277d8e37131913ff1
Shreejal Dhungana <dshreejal@gmail.com>
2023-02-02 21:54:56 +0545

    css changes

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -28,7 +28,7 @@ const Blog = ({ blog }) => {
                     <p className="text-gray-700 text-base mb-4">
                         {truncateString(blog?.description, 30)}
                     </p>
-                    <div className='flex justify-between'>
+                    <div className='flex  justify-around gap-4'>
                         <button type="button" className=" inline-block px-6 py-2.5 bg-indigo-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-indigo-700 hover:shadow-lg focus:bg-indigo-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-indigo-800 active:shadow-lg transition duration-150 ease-in-out" onClick={handleReadMe} >Read More</button>
                         <div className='flex items-center justify-center gap-2 text-xs'>
                             <div>
